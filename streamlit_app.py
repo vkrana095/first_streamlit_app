aa3d91289e1b17f9e3e9f7e7ffadc16092aaa041
Vivek Rana <84652124+vkrana095@users.noreply.github.com>
2023-05-07 11:00:02 +0530

    Update streamlit_app.py
    Breakfast favourites added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,16 @@ import streamlit
 
 streamlit.title('My Healthier dinner')
 
-streamlit.header('Breakfast Menu') 
+# streamlit.header('Breakfast Menu') 
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+# streamlit.text('Omega 3 & Blueberry Oatmeal')
+# streamlit.text('Kale, Spinach & Rocket Smoothie')
+# streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('Breakfast Favourites') 
+
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
