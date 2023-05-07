d9558de07b14901430dea8a009536dca8d9ecf5d
Vivek Rana <84652124+vkrana095@users.noreply.github.com>
2023-05-07 11:33:02 +0530

    Update streamlit_app.py
    Adding Pd df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Healthier dinner')
 
@@ -17,3 +18,6 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
