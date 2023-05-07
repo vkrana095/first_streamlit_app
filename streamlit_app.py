097960f35f006367eba8e8808313b07580e6005f
Vivek Rana <84652124+vkrana095@users.noreply.github.com>
2023-05-07 10:53:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Healthier dinner')
 streamlit.header('Breakfast Menu') 
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale', 'Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
