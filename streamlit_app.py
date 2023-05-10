3b3866d4613aadbe5058849fddae47120a7882f6
Vivek Rana <84652124+vkrana095@users.noreply.github.com>
2023-05-10 09:36:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #picker
 # streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_selected = streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
@@ -33,3 +34,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# write your own comment -It will extract json with pandas normalization? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - pandas will show the normalized data frame from jason?
+streamlit.dataframe(fruityvice_normalized)
