a8dde8f9b30eb2ab08ad140b788f63f0ef2efbbb
Vivek Rana <84652124+vkrana095@users.noreply.github.com>
2023-05-10 09:40:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+# streamlit.text(fruityvice_response.json())
 
 # write your own comment -It will extract json with pandas normalization? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
