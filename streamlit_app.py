f844da0ba6af3cb06228be9b0ea61450ddb07f1d
Vivek Rana <84652124+vkrana095@users.noreply.github.com>
2023-05-13 11:04:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,16 +17,16 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-
+my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
+my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 #picker
 # streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_selected = streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
+my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
@@ -41,7 +41,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - pandas will show the normalized data frame from jason?
 streamlit.dataframe(fruityvice_normalized)
-
+my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -52,3 +52,4 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
